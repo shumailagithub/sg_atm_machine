d969d550ef572ea75e023d233ea10dd2b49cd81f
shumailagithub <163028638+shumailagithub@users.noreply.github.com>
2024-04-04 12:49:45 +0500

    Add files via upload

---
+++ atm.ts
@@ -0,0 +1,74 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+
+let totalBalance:number = 10000
+const pinNumber:number = 1234
+
+let pinEntered = await inquirer.prompt([{
+    name: "pin",
+    type: "number",
+    message: "Enter 4 digit code"
+}])
+
+//console.log(pinEntered.pin);
+
+if (pinEntered.pin === pinNumber) {
+
+    let atmQuestions =await inquirer.prompt([{
+        name: "accounType",
+        type: "list",
+        message: "select your account type: ",
+        choices: ["Current Account", "Saving Account",]
+    },
+{
+    name: "transMethod",
+    type: "list",
+    message: "Select your transaction Method",
+    choices: [
+        "Cash Withdrawal", "Fast Cash"
+    ]
+}
+]);
+if(atmQuestions.transMethod == "Cash Withdrawal")
+{
+
+    let cashwithrawalAmount = await inquirer.prompt([{
+        name: "withdrawal",
+        message: "Enter the amount you want to withdraw",
+        type: "number",
+    }
+]);
+      // Greater than or equals to operator
+if(totalBalance >= cashwithrawalAmount.withdrawal){
+
+totalBalance -= cashwithrawalAmount.withdrawal  //totalBalance = totalBalance - cashwithrawalAmount
+console.log(`your Total Balance is : ${totalBalance}`);
+
+}
+else{
+    console.log(`Insufficient Balance`);
+    
+}
+
+}
+else{
+    let fastCashAmount = await inquirer.prompt([{
+        name: "fastCash",
+        type: "list",
+        message: "Select Amont you want to withdrawal",
+        choices: [ 1000, 3000, 5000 ]
+    
+    }])
+    if(totalBalance >= fastCashAmount.fastCash){
+        totalBalance -= fastCashAmount.fastCash
+        console.log(`your total Balance is : ${totalBalance}`);
+        
+    }
+    else{
+        console.log(`Insufficient Balance`);
+        
+    }
+}    
+}
